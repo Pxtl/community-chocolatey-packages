6ba493ed869bfff6d27089364fb8de50ee7cbf91
Martin Zarate <pxtl@hotmail.com>
2024-06-05 16:20:08 -0400

    indent using tabs

--- packages/standalone-ssis-devops-tools/tools/chocolateyinstall.ps1
+++ packages/standalone-ssis-devops-tools/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://download.microsoft.com/download/5/1/4/5144b772-d3b0-4e1c-a05b-5376f2ea0fc1/SSISDevOpsTools-1.0.0.0.exe'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  url           = $url
-  checksum      = '5B721116D982457EF2FB8C584EF8AE24B7266E519FD5F16CAA867FA32EBDD14F'
-  checksumType  = 'sha256'
+	packageName   = $env:ChocolateyPackageName
+	unzipLocation = $toolsDir
+	url           = $url
+	checksum      = '5B721116D982457EF2FB8C584EF8AE24B7266E519FD5F16CAA867FA32EBDD14F'
+	checksumType  = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
